8d82ea08751d91b63d9ded2f4d677ab377a711e5
Serg <blondewalker@gmail.com>
2024-10-11 01:51:33 +0200

    update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -12,27 +12,27 @@ import { TeamSection } from "@/components/layout/sections/team";
 import { TestimonialSection } from "@/components/layout/sections/testimonial";
 
 export const metadata = {
-  title: "Shadcn - Landing template",
-  description: "Free Shadcn landing page for developers",
+  title: "Anyrand VRF",
+  description: "Free verifiable randomness for all EVM compatible blockchains",
   openGraph: {
     type: "website",
-    url: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
+    url: "https://anyrand.com",
+    title: "Anyrand VRF",
+    description: "Free verifiable randomness for all EVM compatible blockchains",
     images: [
       {
         url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
         width: 1200,
         height: 630,
-        alt: "Shadcn - Landing template",
+        alt: "Anyrand VRF",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    site: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
+    site: "https://anyrand.com",
+    title: "Anyrand VRF",
+    description: "Free verifiable randomness for all EVM compatible blockchains",
     images: [
       "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
     ],
