db09cc1c92da0306d4236638d74e8a393c6ab3b7
Serg <blondewalker@gmail.com>
2024-10-13 07:32:43 +0200

    add sticky mint button

--- app/page.tsx
+++ app/page.tsx
@@ -52,6 +52,25 @@ export default function Home() {
       <PricingSection />
       <FAQSection />
       <FooterSection />
+      <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
+        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-gray-900 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
+          <p className="text-sm leading-6 text-white">
+            <a href="#">
+              <strong className="font-semibold">Mint NOW!</strong>
+              <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline h-0.5 w-0.5 fill-current">
+                <circle r={1} cx={1} cy={1} />
+              </svg>
+              Celebrate Scroll EVM launch with us and mint commemorative dice NFT!&nbsp;<span aria-hidden="true">&rarr;</span>
+            </a>
+          </p>
+          <button type="button" className="-m-1.5 flex-none p-1.5">
+            <span className="sr-only">Dismiss</span>
+            <svg className="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+              <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+            </svg>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
