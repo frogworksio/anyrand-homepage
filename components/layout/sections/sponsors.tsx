6b24c8a8d7ffd93b9f2777e1e6ad438d9ca2798d
Serg <blondewalker@gmail.com>
2024-10-13 07:32:04 +0200

    add coming soon section

--- components/layout/sections/sponsors.tsx
+++ components/layout/sections/sponsors.tsx
@@ -1,69 +1,62 @@
 "use client";
 
-import { Icon } from "@/components/ui/icon";
 import { Marquee } from "@devnomic/marquee";
 import "@devnomic/marquee/dist/index.css";
-import { icons } from "lucide-react";
+
 interface sponsorsProps {
-  icon: string;
+  image: string; // Updated from 'icon' to 'image'
   name: string;
 }
 
+// Replace the 'icon' field with 'image', pointing to the images in the public folder
 const sponsors: sponsorsProps[] = [
   {
-    icon: "Crown",
-    name: "Acmebrand",
+    image: "logo-eth.svg",
+    name: "coming soon",
   },
   {
-    icon: "Vegan",
-    name: "Acmelogo",
+    image: "logo-base.svg",
+    name: "coming soon",
   },
   {
-    icon: "Ghost",
-    name: "Acmesponsor",
+    image: "logo-scroll.svg",
+    name: "live on Scroll",
   },
   {
-    icon: "Puzzle",
-    name: "Acmeipsum",
+    image: "logo-gnosis.svg",
+    name: "coming soon",
   },
   {
-    icon: "Squirrel",
-    name: "Acme",
-  },
-  {
-    icon: "Cookie",
-    name: "Accmee",
-  },
-  {
-    icon: "Drama",
-    name: "Acmetech",
+    image: "logo-op.svg",
+    name: "coming soon",
   },
 ];
 
 export const SponsorsSection = () => {
   return (
-    <section id="sponsors" className="max-w-[75%] mx-auto pb-24 sm:pb-32">
-      <h2 className="text-lg md:text-xl text-center mb-6">
-        Our Platinum Sponsors
+    <section id="sponsors" className="container w-full mt-20 mb-20">
+      <h2 className="tracking-tight text-lg md:text-xl text-center mb-8">
+        Anyrand VRF coming to
       </h2>
 
       <div className="mx-auto">
         <Marquee
-          className="gap-[3rem]"
+          className="gap-[3rem] [--duration:60s]"
           fade
           innerClassName="gap-[3rem]"
           pauseOnHover
         >
-          {sponsors.map(({ icon, name }) => (
+          {sponsors.map(({ image, name }) => (
             <div
               key={name}
               className="flex items-center text-xl md:text-2xl font-medium"
             >
-              <Icon
-                name={icon as keyof typeof icons}
-                size={32}
-                color="white"
+              <img
+                src={image} // Use the image path instead of icon component
+                alt={name}
                 className="mr-2"
+                height={24}
+                width={24}
               />
               {name}
             </div>
