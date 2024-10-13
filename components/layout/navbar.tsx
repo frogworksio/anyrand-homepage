92f9d9ab8dd20c149f5ae87dcc87ff50edd42ba7
Serg <blondewalker@gmail.com>
2024-10-13 07:32:30 +0200

    add anyrand logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -18,6 +18,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "../ui/navigation-menu";
+import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import Image from "next/image";
@@ -70,12 +71,20 @@ const featureList: FeatureProps[] = [
 ];
 
 export const Navbar = () => {
+  const { theme } = useTheme();
   const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <header className="backdrop-blur shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2">
+    <header className="backdrop-blur bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky z-40 rounded-2xl flex justify-between items-center p-2">
       <Link href="/" className="font-bold text-lg flex items-center">
-        <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-        Shadcn
+        <img
+          alt="Logo anyrand"
+          src={
+            theme === "dark"
+              ? "/anyrand-logo-white.svg"
+              : "/anyrand-logo-dark.svg"
+          }
+          className="max-h-20 w-full object-contain"
+        />
       </Link>
       {/* <!-- Mobile --> */}
       <div className="flex items-center lg:hidden">
@@ -95,8 +104,15 @@ export const Navbar = () => {
               <SheetHeader className="mb-4 ml-4">
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
-                    <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-                    Shadcn
+                    <img
+                      alt="Logo anyrand"
+                      src={
+                        theme === "dark"
+                          ? "/anyrand-logo-white.svg"
+                          : "/anyrand-logo-dark.svg"
+                      }
+                      className="max-h-20 w-full object-contain"
+                    />
                   </Link>
                 </SheetTitle>
               </SheetHeader>
